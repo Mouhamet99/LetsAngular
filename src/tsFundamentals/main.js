db756a2b188d6edce377469531241b6f76957da7
Mouhamet99 <dieyemouhamet1@gmail.com>
2022-09-04 12:36:26 +0000

    Getter and Setter

--- src/tsFundamentals/main.js
+++ src/tsFundamentals/main.js
@@ -6,7 +6,19 @@ var Point = /** @class */ (function () {
     Point.prototype.draw = function () {
         console.log("X:" + this.x + ", Y:" + this.y);
     };
+    Point.prototype.getX = function () {
+        return this.x;
+    };
+    Point.prototype.setX = function (value) {
+        if (value < 0) {
+            throw new Error("value can not be less than zero");
+        }
+        this.x = value;
+    };
     return Point;
 }());
 var point = new Point(1, 2);
+var x = point.getX();
+console.log({ x: x });
+point.setX(10);
 point.draw();
--- src/tsFundamentals/main.ts
+++ src/tsFundamentals/main.ts
@@ -1,12 +1,23 @@
 class Point {
+  constructor(private x?: number, private y?: number) {}
 
-   constructor(private x?: number, private y?: number){
-   }
+  draw() {
+    console.log('X:' + this.x + ', Y:' + this.y);
+  }
 
-   draw (){
-      console.log("X:"+ this.x+", Y:"+ this.y);
-   }
+  getX() {
+    return this.x;
+  }
+
+  setX(value: number) {
+    if (value < 0) {
+      throw new Error('value can not be less than zero');
+    }
+    this.x = value;
+  }
 }
 let point = new Point(1, 2);
-
+let x = point.getX();
+console.log({ x });
+point.setX(10);
 point.draw();
