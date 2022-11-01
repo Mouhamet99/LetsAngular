eb629ffcbeea5785d342823acab484c15b097978
Mouhamet99 <dieyemouhamet1@gmail.com>
2022-11-01 18:49:17 +0000

    FormControl and ngModel directive

---
+++ src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NgModel, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css']
+})
+
+export class ContactFormComponent {
+  log(x: any) {
+    console.log(x);
+  }
+}
