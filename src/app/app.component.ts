f5ab3c2b9ab4579ae4fdf44a5175f5dc35cd6b83
Mouhamet99 <dieyemouhamet1@gmail.com>
2022-11-01 00:28:00 +0000

    explore change detection and ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hello World Angular';
   courses = [
     {title: "Course 1", author: "Epsilon"},
     {title: "Course 2", author: "Soso"},
     {title: "Course 1", author: "Libs"}
   ];
-  viewMode :string = "map";
 
+  add(){
+    this.courses.push({title: "Course 4", author: "Rokhaya"})
+  }
+
+  onRemove(course: any){
+    console.log(course);
+    
+    let index = this.courses.indexOf(course)
+    this.courses.splice(index, 1)
+  }
+
+}
+export default interface Course{
+  "title": String, "author": String
 }
 
