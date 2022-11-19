476cadfcb24230a4ddd8e0babf7175418572bd21
Mouhamet99 <dieyemouhamet1@gmail.com>
2022-11-19 18:01:16 +0000

    add password strength progress validator

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -21,6 +21,28 @@ export class SignupFormComponent {
         password: new FormControl('', Validators.required),
     });
 
+    passwordStrengths = {
+        weak: { name: 'weak', color: 'bg-danger', width: '10' },
+        far: { name: 'far', color: 'bg-warning', width: '25' },
+        good: { name: 'good', color: 'bg-secondary', width: '50' },
+        strong: { name: 'strong', color: 'bg-success', width: '100' },
+    };
+
+    currentPasswordStrength = { name: 'weak', color: 'bg-danger', width: '10' };
+
+    handlePasswordStrength(password: any) {
+        const passwordValue = password.value as string;
+
+        this.currentPasswordStrength =
+            passwordValue.length >= 10
+                ? this.passwordStrengths.strong
+                : passwordValue.length >= 8
+                ? this.passwordStrengths.good
+                : passwordValue.length >= 4
+                ? this.passwordStrengths.far
+                : this.passwordStrengths.weak;
+    }
+
     get username() {
         return this.form.get('username');
     }
