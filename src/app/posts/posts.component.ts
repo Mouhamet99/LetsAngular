e62380a72d3acd8b50775baa1847ff9fdb0dfa3f
Mouhamet99 <dieyemouhamet1@gmail.com>
2022-11-20 14:12:46 +0000

    throwing app specific errors

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,57 +1,78 @@
 import { HttpHeaderResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AppError } from '../common/app-error';
+import { NotFoundError } from '../common/not-found-error';
 import { PostsService } from './posts.service';
 
 @Component({
-  selector: 'posts',
-  templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css'],
+    selector: 'posts',
+    templateUrl: './posts.component.html',
+    styleUrls: ['./posts.component.css'],
 })
 export class PostsComponent implements OnInit {
-  posts: Post[] = [];
-  headers: string[] = [];
+    posts: Post[] = [];
+    headers: string[] = [];
+    error: any = [];
 
-  constructor(private postService: PostsService) {
+    constructor(private postService: PostsService) {}
+
+    ngOnInit() {
+        this.postService.getPosts().subscribe((resp) => {
+            const keys = resp.headers.keys();
+            this.headers = keys.map(
+                (key) => `${key}: ${resp.headers.get(key)}`
+            );
+
+            this.posts = [...resp.body!];
+        });
+    }
+
+    createPost(input: HTMLInputElement) {
+        let post: any = { title: input.value, body: input.value };
+        this.postService.createPost(post).subscribe((res) => {
+            post.id = res.id;
+            this.posts.unshift(post);
+        });
+    }
+
+    updatePost(post: Post) {
+        this.postService.updatePost(post).subscribe((res) => {
+            console.log('Update Post response', res);
+        });
+    }
+
+    deletePost(post: Post) {
+        this.postService.deletePost(post.id).subscribe({
+            next: (res) => {
+                let index = this.posts.indexOf(post);
+                this.posts.splice(index, 1);
+                console.log('Delete post response', res);
+            },
+            error: (error: AppError) => {
+                if (error instanceof NotFoundError)
+                    this.error = error.originalError;
+            },
+        });
+    }
     
-  }
-
-  ngOnInit(){
-    this.postService.getPosts().subscribe((resp) => {
-      const keys = resp.headers.keys();
-      this.headers = keys.map((key) => `${key}: ${resp.headers.get(key)}`);
-
-      this.posts = [...resp.body!];
-    });
-
-  }
-
-  createPost(input: HTMLInputElement) {
-    let post: any = { title: input.value, body: input.value };
-    this.postService.createPost(post).subscribe((res) => {
-      post.id = res.id;
-      this.posts.unshift(post);
-    });
-  }
-
-  updatePost(post:Post){
-    this.postService.updatePost(post).subscribe((res) => {
-      console.log("Update Post response", res);
-    })
-  }
-
-  deletePost(post:Post){
-    this.postService.deletePost(post.id).subscribe(res=>{
-      let index = this.posts.indexOf(post)
-      this.posts.splice(index, 1)
-      
-      console.log("Delete post response", res)
-    })
-  }
+    badDeletePost(post: Post) {
+        this.postService.badDeletePost(post.id).subscribe({
+            next: (res) => {
+                let index = this.posts.indexOf(post);
+                this.posts.splice(index, 1);
+                console.log('Delete post response', res);
+            },
+            error: (error: AppError) => {
+                if (error instanceof NotFoundError)
+                    this.error = error.originalError;
+            },
+        });
+    }
 }
 
 export interface Post {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
 }
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -3,50 +3,65 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import { Post } from './posts.component';
+import { NotFoundError } from '../common/not-found-error';
+import { AppError } from '../common/app-error';
 
 @Injectable()
 export class PostsService {
-  private url = 'http://jsonplaceholder.typicode.com/posts';
-  constructor(private http: HttpClient) {}
-  private handleError(error: HttpErrorResponse) {
-    if (error.status === 0) {
-      alert('A client-side or network error occurred: check console');
-      console.log('An error occurred: ' + error.error);
-    } else {
-        // this.form.setErrors(error)
-      alert(`The backend returned an unsuccessful response code: check console`);
-      console.error(
-        `Backend returned code ${error.status}, body was: `,
-        error.error
-      );
+    private url = 'http://jsonplaceholder.typicode.com/posts';
+
+    constructor(private http: HttpClient) { }
+
+    private handleError(error: HttpErrorResponse) {
+        if (error.status === 0) {
+            alert('A client-side or network error occurred: check console');
+            console.log('An error occurred: ' + error.error);
+        } else if (error.status === 404){
+            return throwError(
+                () => new NotFoundError('Post not Found')
+            );
+        } else {
+            // this.form.setErrors(error)
+            alert(
+                `The backend returned an unsuccessful response code: check console`
+            );
+            console.error(
+                `Backend returned code ${error.status}, body was: `,
+                error.error
+            );
+        }
+
+        // Return an observable with a user-facing error message.
+        return throwError(
+            () => new AppError('Something bad happened. Please, try again')
+        );
     }
 
-    // Return an observable with a user-facing error message.
-    return throwError(
-      () => new Error('Something bad happened. Please, try again')
-    );
-  }
+    getPosts() {
+        // const  options = {
+        //     observe: 'body' | 'events' | 'response',
+        //     responseType: 'arraybuffer'|'blob'|'json'|'text',
+        // }
+        return this.http
+            .get<Post[]>(this.url, { observe: 'response' })
+            .pipe(catchError(this.handleError));
+    }
 
-  getPosts() {
-    // const  options = {
-    //     observe: 'body' | 'events' | 'response',
-    //     responseType: 'arraybuffer'|'blob'|'json'|'text',
-    // }
-    return this.http
-      .get<Post[]>(this.url+"l", { observe: 'response' })
-      .pipe(catchError(this.handleError));
-  }
+    createPost(post: { title: string }): Observable<{ id: number }> {
+        return this.http.post<Post>(this.url, post);
+    }
 
-  createPost(post: { title: string }): Observable<{ id: number }> {
-    return this.http.post<Post>(this.url, post);
-  }
+    updatePost(post: Post): Observable<Post> {
+        post.title = 'post update';
+        return this.http.put<Post>(this.url + '/' + post.id, post);
+    }
 
-  updatePost(post: Post): Observable<Post> {
-    post.title = 'post update';
-    return this.http.put<Post>(this.url + '/' + post.id, post);
-  }
-
-  deletePost(id: number): Observable<unknown> {
-    return this.http.delete(this.url + '/' + id);
-  }
+    deletePost(id: number): Observable<unknown> {
+        return this.http.delete(this.url + '/' + id).pipe(catchError(this.handleError));
+    }
+    
+    badDeletePost(id: number): Observable<unknown> {
+        return this.http.delete(this.url).pipe(catchError(this.handleError));
+    }
+   
 }
